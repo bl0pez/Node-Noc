33347e73ce58151527500348e2f9bc4dbf6c0a12
bryan lopez <blopez4434@gmail.com>
2024-01-29 03:53:05 -0300

    Add logging functionality to CheckService and use FileSystemDataSource for LogRepository

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,11 @@
-import { CronJob } from "cron";
 import { CronService } from "./cron/cron-service";
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
+import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+  new FileSystemDataSource()
+);
 
 export class Server {
   public static start(): void {
@@ -9,6 +14,7 @@ export class Server {
     CronService.createJob("*/5 * * * * *", () => {
       const url = "http://localhost:3000";
       new CheckService(
+        fileSystemLogRepository,
         () => console.log(`Success on check service: ${url}`),
         (error) => console.log(`Error on check service: ${error}`)
       ).execute(url);
